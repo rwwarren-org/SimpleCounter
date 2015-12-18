e4a7326d11517058c2388c056f4cbf6289c9c08d
Ryan Warren <ryan@war.re>
2015-12-18 02:14:24 -0800

    simple start

--- index.ios.js
+++ index.ios.js
@@ -10,22 +10,69 @@ var {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
+  AsyncStorage,
 } = React;
 
 var SimpleCounter = React.createClass({
+  componentDidMount: function() {
+    AsyncStorage.getItem("count").then((value) => {
+      if(value === null) {
+        value = 0;
+      }
+      console.log(parseInt(value));
+      this.setState({count: parseInt(value)});
+      //this.setState({count: parseInt(value)});
+    }).done();
+  },
+  getInitialState: function() {
+    return {
+      count: 0,
+    };
+  },
+  _onAdd: function(){
+    this.setState({
+      count: this.state.count + 1,
+    });
+  },
+  _onSubtract: function(){
+    if(this.state.count > 0){
+      this.setState({
+        count: this.state.count - 1,
+      });
+    }
+  },
+  _onReset: function(){
+    this.setState({
+      count: 0,
+    });
+  },
+  saveData: function() {
+    AsyncStorage.setItem("count", '' + this.state.count);
+  },
   render: function() {
+    this.saveData();
+    console.log(this.state.count);
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.count}>
+          {this.state.count}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableHighlight onPress={this._onSubtract} style={styles.roundbuttoncircle}>
+          <Text style={styles.middleText}>
+            -
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._onAdd} style={styles.roundbuttoncircle}>
+          <Text style={styles.middleText}>
+            +
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._onReset} style={styles.roundbuttoncircle}>
+          <Text style={styles.middleText}>
+            Reset
+          </Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -38,15 +85,23 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  count: {
+    fontSize: 100,
   },
-  instructions: {
+  roundbuttoncircle: {
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+        backgroundColor: '#4679BD',
+        justifyContent: 'center',
+        borderColor: '#cfdcec',
+        borderWidth: 10,
+  },
+  middleText: {
+    fontSize: 50,
+    justifyContent: 'center',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'rgba(0,0,0,0)',
   },
 });
 
