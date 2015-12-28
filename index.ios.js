22d9da9d0f9269f316718d98407678b9eea0a43b
Ryan Warren <ryan@war.re>
2015-12-28 00:20:37 -0800

    working on areyousure feautre

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,10 @@ var SimpleCounter = React.createClass({
         value = 0;
       }
       console.log(Number.parseInt(value));
-      this.setState({count: parseInt(value)});
+      this.setState({
+        count: parseInt(value),
+        areYouSure: false,
+      });
     }).done();
   },
   getInitialState: function() {
@@ -28,6 +31,7 @@ var SimpleCounter = React.createClass({
   _onAdd: function(){
     this.setState({
       count: this.state.count + 1,
+      areYouSure: false,
     });
     this.saveData();
   },
@@ -36,15 +40,32 @@ var SimpleCounter = React.createClass({
       this.saveData();
       this.setState({
         count: this.state.count - 1,
+        areYouSure: false,
       });
       this.saveData();
+    } else {
+      this.setState({
+        areYouSure: false,
+      });
     }
   },
-  _onReset: function(){
+  _onReset: function() {
     this.setState({
-      count: 0,
+      //count: 0,
+      areYouSure: true,
     });
     this.saveData();
+  },
+  _areYouSure: function() {
+    if(this.state.areYouSure) {
+      return(
+          <View>
+            <Text>
+            asdf
+            </Text>
+          </View>
+      );
+    }
   },
   saveData: function() {
     console.log("saving: " + this.state.count);
@@ -56,21 +77,24 @@ var SimpleCounter = React.createClass({
         <Text style={styles.count}>
           {this.state.count}
         </Text>
-        <TouchableHighlight onPress={this._onSubtract} style={styles.roundbuttoncircle}>
-          <Text style={styles.middleText}>
-            -
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._onAdd} style={styles.roundbuttoncircle}>
-          <Text style={styles.middleText}>
-            +
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._onReset} style={styles.roundbuttoncircle}>
-          <Text style={styles.middleText}>
+        <View style={styles.buttons}>
+          <TouchableHighlight onPress={this._onSubtract} style={styles.roundbuttoncircle}>
+            <Text style={styles.middleText}>
+              -
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this._onAdd} style={styles.roundbuttoncircle}>
+            <Text style={styles.middleText}>
+              +
+            </Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight onPress={this._onReset} style={styles.roundbuttonreset}>
+          <Text style={styles.resetText}>
             Reset
           </Text>
         </TouchableHighlight>
+        {this._areYouSure}
       </View>
     );
   }
@@ -87,16 +111,44 @@ var styles = StyleSheet.create({
     fontSize: 100,
   },
   roundbuttoncircle: {
-        width: 150,
-        height: 150,
+        width: 120,
+        height: 120,
         borderRadius: 150 / 2,
         backgroundColor: '#4679BD',
         justifyContent: 'center',
         borderColor: '#cfdcec',
         borderWidth: 10,
+        alignSelf: 'auto',
+        marginHorizontal: 5,
+  },
+  roundbuttonreset: {
+        width: 80,
+        height: 80,
+        borderRadius: 150 / 2,
+        backgroundColor: '#4679BD',
+        justifyContent: 'center',
+        borderColor: '#cfdcec',
+        borderWidth: 10,
+        //flexDirection: 'column',
+        //flex: 1,
+        //alignItems: 'center',
+        //flexDirection: 'row',
+  },
+  buttons: {
+        height: 200,
+        flexDirection: 'column',
+        //flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
   },
   middleText: {
     fontSize: 50,
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0)',
+  },
+  resetText: {
+    fontSize: 20,
     justifyContent: 'center',
     textAlign: 'center',
     backgroundColor: 'rgba(0,0,0,0)',
